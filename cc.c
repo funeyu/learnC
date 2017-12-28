43f4c65fe957e9eb266377e6ed8c8019cb8aed43
funeyu <funeyu@gmail.com>
2017-12-28 23:59:06 +0800

    function arg bug

--- cc.c
+++ cc.c
@@ -253,7 +253,7 @@ void print_ast(Ast *ast) {
             break;
         case AST_FUNCALL:
             printf("%s(", ast->fname);
-            for (int i = 0; ast->args[i]; i ++) {
+            for (int i = 0; args[i]; i ++) {
                 print_ast(ast->args[i]);
                 if(ast->args[i + 1])
                     printf(",");
